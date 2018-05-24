dd2b0d70c49672f842d8846f0ddc295e1709cc15
organwolf <aron.polner@gmail.com>
2018-05-24 19:36:47 +0200

    Removed TWI_Command and added comments

--- src/I2C_Client.c
+++ src/I2C_Client.c
@@ -4,7 +4,7 @@
 *  Drivers for ATmega2560 as I2C client.
 *
 *  Created: 2018-05-01 14:37:40
-*  Author: SleepyOak, Filip Nilsson, Aron Polner
+*  Author: SleepyOak, Filip Nilsson, Aron Polner.
 */
 
 #include <asf.h>
@@ -14,17 +14,20 @@
 #include "uart.h"
 
 #define F_CPU 16000000UL
-#define nbrOfBytes 10		//When the master requests, the client returns 10bytes
+#define nbrOfBytes 10							// the number of Bytes per communication with the master I2C device
 
 
 volatile uint8_t TWI_Command = 1;
-volatile uint8_t TWI_datatrack = 0;				//tracks how much data has been sent
+volatile uint8_t TWI_datatrack = 0;				// tracks how much data has been sent
 
-/* 
-*  This functions ---
-*  describe the address 
-*
-*/
+/************************************************************************/
+/* The function I2C_Client_Init() takes an uint8_t address and	        */
+/* Enables interrupt T1 (PD6) as well as SCL (PD0) and SDA (PD1).		*/
+/*																		*/
+/* Further the global interrupt is enabled, the address loaded in to	*/
+/* TWAR (the address register) and lastly TWI is enables along with		*/ 
+/* the TWI interrupt flag.												*/
+/************************************************************************/
 void I2C_Client_Init(uint8_t address){
 	
 	ioport_set_port_dir(IOPORT_PORTD, (1<<PD6), IOPORT_DIR_OUTPUT);
@@ -35,57 +38,54 @@ void I2C_Client_Init(uint8_t address){
 
 	ioport_set_port_dir(IOPORT_PORTD, (1<<PD1), IOPORT_DIR_OUTPUT);
 	ioport_set_port_level(IOPORT_PORTD, (1<<PD1), IOPORT_PIN_LEVEL_HIGH);	//pull-up SDA
-	
-	//Enable global Interrupts
+
 	sei();
-	//Set the client address				
+				
 	TWAR = (address<<1);	
-	// Enable TWI and TWI interrupt flag
 	TWCR = (1<<TWEN)|(1<<TWIE)|(1<<TWEA)|(1<<TWINT);
 }
 
-/*
-*  This interrupt service routine ---
-*
-*
-*/
+/************************************************************************/
+/* This interrupt service routine handles the TWI communication         */
+/*																		*/
+/* The client (ATmega2560) is first prepared to be addressed by the 	*/
+/* master device. Thereafter if the slave address is followed by a read */
+/* command the client sends one byte of data from the pos[] buffer.		*/
+/* If the byte sent is ACKED by the master this continues until 10		*/
+/* bytes have been sent and NACKED.									    */
+/*																		*/
+/* If anything else should happen the TWCR (control register) is		*/
+/* cleared and the I2C client is once again prepared for a new			*/
+/* addressing.															*/
+/************************************************************************/
 ISR(TWI_vect){
-	//UCSR1B |= ~(1<<RXCIE1); //Do we need to stop the USART1 interrupt... 
-	// set pin to low if interrupt is called
+
 	ioport_set_port_level(IOPORT_PORTD, (1<<PD6), IOPORT_PIN_LEVEL_LOW);
 	
-	if (TW_STATUS == TW_SR_STOP){		// stop command or repeated start, set slave to prepare to be addressed. if repeated start likely going to be addressed again but as transmitter
+	if (TW_STATUS == TW_SR_STOP){
 		TWCR |= (1<<TWINT)|(1<<TWEA)|(1<<TWEN);
-
-		} else if (TW_STATUS == TW_ST_SLA_ACK) {	// if slave address comes up with read command (slave is transmitter)
 		
-		if (TWI_Command == 1){					// commands 1 (currently) are the commands that require transmission
-			TWDR = pos[TWI_datatrack];
-			TWI_datatrack++;
-			TWCR = (1<<TWINT)|(0<<TWSTO)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE);
+	} else if (TW_STATUS == TW_ST_SLA_ACK){	
+				TWDR = pos[TWI_datatrack];
+				TWI_datatrack++;
+				TWCR = (1<<TWINT)|(0<<TWSTO)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE);
 
-			} else {								// if not one of these commands then the system has been commanded incorrectly, set to send 0xFF and receive nack
-			TWDR = 0xFF;
-			TWCR = (1<<TWINT)|(0<<TWSTO)|(0<<TWEA)|(1<<TWEN)|(1<<TWIE);
-		}
-
-		} else if (TW_STATUS == TW_ST_DATA_ACK){	// if there is still more transmitting to do
+	} else if (TW_STATUS == TW_ST_DATA_ACK){
 		TWDR = pos[TWI_datatrack];
 		TWI_datatrack++;
 		
-		if (TWI_datatrack == nbrOfBytes) {// is this data byte the last one to be sent?, if yes prepare to receive nack
+		if (TWI_datatrack == nbrOfBytes){
 			TWI_datatrack = 0;
-			TWCR = (1<<TWINT)|(0<<TWSTO)|(0<<TWEA)|(1<<TWEN)|(1<<TWIE);
-			//UCSR1B |= (1<<RXCIE1); //Do we need to start the USART1 interrupt... 
+			TWCR = (1<<TWINT)|(0<<TWSTO)|(0<<TWEA)|(1<<TWEN)|(1<<TWIE); 
 
-			} else {
-			TWCR = (1<<TWINT)|(0<<TWSTO)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE);
+		} else{
+		TWCR = (1<<TWINT)|(0<<TWSTO)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE);
 		}
 
-		} else if (TW_STATUS == TW_ST_LAST_DATA) { // if last data has been sent and ack received when it shouldn't have, switch to not addressed
-		TWCR = (1<<TWINT)|(0<<TWSTA)|(0<<TWSTO)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE);
+	} else if (TW_STATUS == TW_ST_LAST_DATA){
+	TWCR = (1<<TWINT)|(0<<TWSTA)|(0<<TWSTO)|(1<<TWEA)|(1<<TWEN)|(1<<TWIE);
 
-		} else {									// (command not recognized, state not accounted for), clear flag, set to prepare for next addressing
-		TWCR |= (1<<TWINT)|(1<<TWEA)|(1<<TWEN);
+	} else {									
+	TWCR |= (1<<TWINT)|(1<<TWEA)|(1<<TWEN);
 	}
 }
